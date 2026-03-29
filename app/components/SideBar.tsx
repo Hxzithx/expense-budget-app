06f8a7335c4bbc6cd2351c64b4247777fdd7647e
Hxzithx <hasithalakruwan081@gmail.com>
2026-03-29 10:00:39 +0530

    fixed nav bar

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,4 +1,4 @@
-"Use client"
+"use client"
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
@@ -9,3 +9,36 @@ const navLinks = [
     { label: "Expenses", href: "/expenses" },
     { label: "History", href: "/history" },
 ]
+
+export default function SideBar() {
+    const pathname = usePathname();
+
+    return (
+        <div className="w-56 min-h-screen bg-gray-900 text-white flex flex-col p-4">
+
+     
+        <div className="mb-8 mt-2">
+            <h1 className="text-x1 font-bold text-white">Budget App</h1>
+            <p className="text-gray-400 text-xs mt-1">Track Your Money</p>
+        </div>
+        {/*nav links*/}
+        <nav className="flex flex-col gap-1">
+            {navLinks.map((link) => {
+                const isActive = pathname === link.href
+                return (
+                    <Link 
+                    key={link.href} 
+                    href={link.href} 
+                    className={`px-4 py-2 rounded-lg text-sm transition-colors ${
+                     isActive
+                     ? "bg-blue-600 text-white font-medium"
+                     : "text-gray-400 hover:bg-gray-500 hover:text-white"
+                    }`}>
+                        {link.label}
+                    </Link>
+                )
+            })}
+        </nav>
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SideBar from "@/app/components/SideBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex bg-gray-100 min-h-screen">
+        <SideBar/>
+        <main className="flex-1 p-8">{children}</main>
+      </body>
     </html>
   );
 }
